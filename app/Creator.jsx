17f08c069ffb44420aca0c72d4d5e9c1b27e4a05
vipankumararya <vipankumararya@gmail.com>
2023-11-18 19:47:03 +1100

    info added

--- app/Creator.jsx
+++ app/Creator.jsx
@@ -5,6 +5,7 @@ export default function Creator() {
         <>
             <div className="text-center mb-8">
                 <p>Created By Vipan Kumar Arya</p>
+                <p>Below Data is fetched from an API and is not a static data. </p>
             </div>
         </>
     );
