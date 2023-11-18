3f53991c1c605b1ea16bff6a885265c5ca5daaf2
vipankumararya <vipankumararya@gmail.com>
2023-11-18 19:37:46 +1100

    fixed linting error

---
+++ app/Creator.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Creator() {
+    return (
+        <>
+            <div className="text-center mb-8">
+                <p>Created By Vipan Kumar Arya</p>
+            </div>
+        </>
+    );
+}
